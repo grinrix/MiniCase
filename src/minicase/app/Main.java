3322c3b60683fbf1b684b36f242674ec227a4b3f
grinrix <146460587+grinrix@users.noreply.github.com>
2025-12-03 18:04:26 +0100

    klasy abstrakcyjne dla itemów oraz ich rzadkość

---
+++ src/minicase/app/Main.java
@@ -0,0 +1,10 @@
+package minicase.app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MiniCase");
+
+        // Tu będzie testowanie kodu
+        System.out.println("sigma");
+    }
+}
